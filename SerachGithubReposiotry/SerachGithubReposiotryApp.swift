708f7d2761cfee77fb44164d28a02b195b6e726c
gmehedi <mehedi.self@gmail.com>
2023-03-19 20:39:09 +0600

    [Create] Github repository project created with SwiftUI

---
+++ SerachGithubReposiotry/SerachGithubReposiotryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SerachGithubReposiotryApp.swift
+//  SerachGithubReposiotry
+//
+//  Created by M M Mehedi Hasan on 19/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SerachGithubReposiotryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchRepositoryView()
+        }
+    }
+}
